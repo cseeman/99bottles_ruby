7739ac668920fd57d6228dfa209214b3b909993a
Christine Seeman <christine.seeman@wpengine.com>
2022-08-26 16:41:00 -0500

    4.10 Finished Refactoring
    Attempted to add in the test around the six pack but wasn't quite done
    with the refactoring work here. Still feels very duplicated with all
    the small methods, but they are very single purpose ones that are
    better for testing, and understanding the concepts.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -9,29 +9,49 @@ class Bottles
 
   def container(number)
     if number == 1
-      "bottle"
+      "bottle"    
     else
       "bottles"
     end
   end
 
-  def verse(number) 
-    case number 
-    when 0
-      "No more bottles of beer on the wall, " + "no more bottles of beer.\n" +
-        "Go to the store and buy some more, " +
-        "99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, " +
-        "1 bottle of beer.\n" +
-        "Take it down and pass it around, " + 
-        "no more bottles of beer on the wall.\n"
+  def pronoun(number)
+    if number == 1
+      "it"
     else
-      "#{number} bottles of beer on the wall, " +
-        "#{number} bottles of beer.\n" +
-        "Take one down and pass it around, " + 
-        "#{number-1} #{container(number-1)} of beer on the wall.\n"
+      "one"
     end
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
+  end
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
+    else 
+      "Take #{pronoun(number)} down and pass it around" 
+    end
+  end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
+  
+  def verse(number) 
+    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+    "#{quantity(number)} #{container(number)} of beer.\n" +
+    "#{action(number)}, " + 
+    "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
   end
 end
 
